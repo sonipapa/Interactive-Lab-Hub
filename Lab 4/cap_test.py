585fa5873e42d9155ec3fbbf8f1f13f2b3acd9fd
Songyu Du <songyudu@nyu.edu>
2021-04-05 15:43:09 -0400

    Upload lab4

--- Lab 4/cap_test.py
+++ Lab 4/cap_test.py
@@ -1,6 +1,8 @@
 import time
 import board
 import busio
+import vlc
+
 
 import adafruit_mpr121
 
@@ -8,8 +10,15 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+keys = ["C","#C","D","#D","E","F","#F","G","#G","A","#A","B"]
+
 while True:
-    for i in range(12):
-        if mpr121[i].value:
-            print(f"Banana {i} touched!")
-    time.sleep(0.25)  # Small delay to keep from spamming output messages.
+	for i in range(12):
+		filepath = "/home/pi/Interactive-Lab-Hub/Lab 4/"+keys[i]+".mp3"
+		p = vlc.MediaPlayer(filepath)
+		if mpr121[i].value:
+			p.play()
+			print(f"{keys[i]} touched!")
+		else:
+			p.stop()
+	time.sleep(0.75)  # Small delay to keep from spamming output messages.
